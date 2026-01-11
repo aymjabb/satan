e6d5f9bedd0c9accbafa9edfa46962a0ef01ae0d
aymjabb <kkayman300@gmail.com>
2026-01-11 23:27:53 +0300

    Create exampleModel.js

---
+++ scr/models/exampleModel.js
@@ -0,0 +1,14 @@
+/**
+ * 🌸 Layla Example Model
+ * Owner: 𝘼𝙮𝙢𝙖𝙣
+ */
+
+class User {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.joinedAt = new Date();
+  }
+}
+
+module.exports = User;
