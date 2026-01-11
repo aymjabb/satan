d6eed033b0dbbd8d3723db41219ea54f102006c5
aymjabb <kkayman300@gmail.com>
2026-01-11 23:34:16 +0300

    Update logger.js

--- scr/utils/logger.js
+++ scr/utils/logger.js
@@ -3,7 +3,26 @@
  * Owner: 𝘼𝙮𝙢𝙖𝙣
  */
 
+const chalk = require("chalk");
+
 module.exports = function logger(message, type = "INFO") {
   const time = new Date().toLocaleTimeString();
-  console.log(`[${type}] [${time}] ${message}`);
+  let color = chalk.white;
+
+  switch (type) {
+    case "INFO":
+      color = chalk.cyan;
+      break;
+    case "WARN":
+      color = chalk.yellow;
+      break;
+    case "ERROR":
+      color = chalk.redBright;
+      break;
+    case "SUCCESS":
+      color = chalk.greenBright;
+      break;
+  }
+
+  console.log(color(`[${type}] [${time}] ${message}`));
 };
